151b4e6455aaf58037351fac0fe0cf94dcea992f
Lamim22 <glamim163@gmail.com>
2022-07-25 17:23:43 -0300

    Add os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { HomeComponent } from './home/home.component';
+import { ItemMenuComponent } from './menu/item-menu/item-menu.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    CarrinhoComponent,
+    HomeComponent,
+    MenuComponent,
+    ItemMenuComponent
   ],
   imports: [
     BrowserModule
